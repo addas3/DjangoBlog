d8adc447f5193c94cf963340eaadaf34f8d9c81f
addas3 <addas3code@gmail.com>
2023-09-25 14:00:24 +0000

    complete add views

--- codstar/urls.py
+++ codstar/urls.py
@@ -1,4 +1,4 @@
-"""codstar URL Configuration
+"""codestar URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
