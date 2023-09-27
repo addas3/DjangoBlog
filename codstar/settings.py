53059f041de4e040ce7bbce39d6d3daf006534cd
addas3 <addas3code@gmail.com>
2023-09-27 18:55:53 +0000

    add comment, like

--- codstar/settings.py
+++ codstar/settings.py
@@ -31,9 +31,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# 8000-addas3-djangoblog-z66uon86s1c.ws-eu104.gitpod.io
-ALLOWED_HOSTS = ['django-blog-codestar-addas3.herokuapp.com', 'localhost', '8000-addas3-djangoblog-z66uon86s1c.ws-eu104.gitpod.io',]
 
+ALLOWED_HOSTS = ['django-blog-codestar-addas3.herokuapp.com', 'localhost', '8000-addas3-djangoblog-z66uon86s1c.ws-eu105.gitpod.io',]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Application definition
 
@@ -43,12 +44,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Add this line
 ]
 
 ROOT_URLCONF = 'codstar.urls'
